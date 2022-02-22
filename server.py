88dd0210a2c10cc200bae5e34c59370aca1d0c46
MikeDxS <maicolsuarez04@gmail.com>
2022-02-21 21:59:02 -0500

    added needed files for heroku

--- server.py
+++ server.py
@@ -63,4 +63,4 @@ def process():
 if __name__ == '__main__':
     if not os.path.exists('./video'):
         os.mkdir('./video')
-    app.run(host='127.0.0.1')
+    app.run(host='0.0.0.0', port=5000)
