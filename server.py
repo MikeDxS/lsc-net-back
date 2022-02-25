3695e609a1f3107ba6dbc3945ec6bf8219e766af
MikeDxS <maicolsuarez04@gmail.com>
2022-02-24 21:44:50 -0500

    removes unnecesary files and changes server.py

--- server.py
+++ server.py
@@ -1,16 +1,36 @@
 import os
+from pathlib import Path
 import tensorflow as tf
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from fastapi import FastAPI, UploadFile, Form, File
+from fastapi.middleware.cors import CORSMiddleware
 from werkzeug.utils import secure_filename
 from keras_video import VideoFrameGenerator
 import numpy as np
 import shutil
+from typing import Optional
+from pydantic import BaseModel
 
-app = Flask(__name__)
-CORS(app)
 
-UPLOAD_FOLDER = '.\\video'
+class Item(BaseModel):
+    userName: str
+    video: UploadFile
+    description: Optional[str] = None
+
+app = FastAPI()
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+UPLOAD_FOLDER = './video'
 ALLOWED_EXTENSIONS = {'mp4'}
 SIZE = (300, 300)
 CHANNELS = 3
@@ -23,25 +43,29 @@ def allowed_file(filename: str) -> bool:
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/")
-def home():
-    return jsonify({"message": "hola"})
+@app.get("/")
+async def home():
+    return {"message": "hola"}
 
-@app.route("/api/process", methods=['POST'])
-def process():
-    if 'video' not in request.files:
-        return jsonify({ 'message': 'The request doesnt have a video file' }), 400
-    jsonBody = request.form
-    video = request.files['video']
+@app.post("/api/process")
+async def process(userName: str = Form(...), video: UploadFile = File(...)):
+    if video == None:
+        return { 'message': 'The request doesnt have a video file' }, 400
     video_path = UPLOAD_FOLDER
     if video.filename == '':
-        return jsonify({ 'message': 'The video file doesnt have a valid name' }), 400
+        return { 'message': 'The video file doesnt have a valid name' }, 400
     if video and allowed_file(video.filename):
+        print('lee nombre video')
         filename = secure_filename(video.filename)
-        class_name = jsonBody['userName']
+        class_name = userName
         os.mkdir(os.path.join(UPLOAD_FOLDER, class_name))
         video_path = os.path.join(UPLOAD_FOLDER, class_name, filename)
-        video.save(video_path)
+        destination = Path(video_path)
+        try:
+            with destination.open("wb") as buffer:
+                shutil.copyfileobj(video.file, buffer)
+        finally:
+            video.file.close()
         glob_pattern = video_path.replace(f"{class_name}", '{classname}')
         train = VideoFrameGenerator(
             classes=[class_name], 
@@ -58,9 +82,4 @@ def process():
         classes = np.around(classes, 4)
         y_hat = np.argmax(classes)
         shutil.rmtree(os.path.join(UPLOAD_FOLDER, class_name))
-        return jsonify({"class": int(y_hat), "predictions": classes.tolist()})
-
-if __name__ == '__main__':
-    if not os.path.exists('./video'):
-        os.mkdir('./video')
-    app.run(host='0.0.0.0', port=5000)
+        return {"class": int(y_hat), "predictions": classes.tolist()}
